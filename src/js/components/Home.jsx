871fab5ddfbfa18caeb65f9936b639216d23674f
bea-bp <129992845+bea-bp@users.noreply.github.com>
2023-07-09 18:42:20 +0000

    landing page with Ract

---
+++ src/js/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+//include images into your bundle
+import { Navbar } from "./Navbar.jsx";
+import { Header } from "./Header.jsx";
+import { CardList } from "./CardList.jsx";
+import { Footer } from "./Footer.jsx";
+import { Card } from "./Card.jsx";
+
+//create your first component
+const Home = () => {
+	return (
+		<div>
+			<Navbar></Navbar>
+			<Header></Header>
+			<CardList></CardList>
+			<Footer></Footer>
+			<Card></Card>
+		</div>
+	);
+};
+
+export default Home;
